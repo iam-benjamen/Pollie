064213f15262ef13b7e78849588318dcd016a36b
Areo Benjamen <areotimileyin1@gmail.com>
2023-05-04 22:17:34 +0100

    (feat): postgres migration

---
+++ database/db.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"github.com/iam-benjamen/Pollie/src/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+	return db
+}
